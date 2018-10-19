891020a9ec41a7d67c362c17b82d1f9cb1b4b45a
lyq <1040541007@qq.com>
2018-10-19 09:52:45 +0800

    merge ls

--- src/scripts/controllers/cinema.js
+++ src/scripts/controllers/cinema.js
@@ -1,16 +1,17 @@
 import cinemaTpl from '../views/cinema.html'
 import cinemaListTpl from '../views/cinema-list.html'
 import cinemaModel from '../models/district'
+import theatreControllers from '../controllers/theatre'
 
 // var datasource = []
 
 const render = async () => {
   let districtList = (await cinemaModel.district()).content.data.page.result
-  let cinemaList = (await cinemaModel.cinemaList()).data.cinemas;
+  let cinemaList = (await cinemaModel.cinemaList()).data.cinemas
   await renderList(districtList, cinemaList)
 }
 
-const renderList = async (districtList, cinemaList) => {
+const renderList = (districtList, cinemaList) => {
   let template = Handlebars.compile(cinemaTpl)
   let html = template({ districtList })
   $('main').html(html)
@@ -18,24 +19,27 @@ const renderList = async (districtList, cinemaList) => {
 }
 
 const cinemaTab = (cinemaList) => {
-  console.log(cinemaList);
-  $('main .district .title').on('tap', function() {
-    $(this).next().html('');
-    $(this).parent().toggleClass('active');
-    let currentCinemaList = [];
-    for(var i = 0; i < cinemaList.length; i++) {
-      if(cinemaList[i].district.pinyin === $(this).parent().attr('data-pinyin')) {
-        currentCinemaList.push(cinemaList[i]);
+  console.log(cinemaList)
+  $('main .district .title').on('tap', function () {
+    $(this).next().html('')
+    $(this).parent().toggleClass('active')
+    let currentCinemaList = []
+    for (let i = 0; i < cinemaList.length; i++) {
+      if (cinemaList[i].district.pinyin === $(this).parent().attr('data-pinyin')) {
+        currentCinemaList.push(cinemaList[i])
       }
     }
     let template = Handlebars.compile(cinemaListTpl)
     let html = template({ currentCinemaList })
     $(this).next().html(html)
+    cinemaClick()
   })
 }
 
-const renderCinema = async ()=>{
-
+const cinemaClick = () => {
+  $('.cinema-wrap').on('tap', function() {
+    theatreControllers.render($(this).attr('data-id'));
+  })
 }
 
 export default {
